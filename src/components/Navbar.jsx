3b991c5600ecfe239ecc2da582a30690c81c8cfe
Sudeep Kumar Srivastava <sudeepsri23@gmail.com>
2022-08-04 23:58:37 +0530

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-primary">
+            <div className="container-fluid">
+                <a className="navbar-brand text-light" href="/Home">Fetch It Baby</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active text-light" aria-current="page" href="/Home">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-light" href="/Weather">Weather</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-light" href="/Movies">Movies</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-light" href="/News">News</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
